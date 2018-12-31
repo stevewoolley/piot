b9b245d2e6991d17abd905d003a2d1fc46ec24a5
Steve Woolley <steve.woolley@me.com>
2018-12-31 09:10:07 -0500

    create subscriber example

--- sub.py
+++ sub.py
@@ -1,66 +1,39 @@
 #!/usr/bin/env python
 
-import logging
-import watchtower
-import time
-import platform
-import argparse
-from gpiozero import DigitalOutputDevice
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-
-TOPIC_STATUS_ON = ['1', 'on']
-TOPIC_STATUS_OFF = ['0', 'off']
-TOPIC_STATUS_TOGGLE = ['toggle']
-TOPIC_STATUS_PULSE = ['blink', 'pulse']
+import time
+import argparse
 
 
-def device(cmd):
-    logging.debug("device {}".format(cmd))
-    if args.pin is not None:
-        if cmd < 0:
-            output.on()
-        elif cmd == 0:
-            output.off()
-        elif cmd > 0:
-            output.blink(args.on_time, args.off_time, cmd)
-
-
-def my_callback(client, user_data, message):
-    logger.debug("callback message {} {}".format(message.topic, message.payload))
-    suffix = message.topic.replace('{}/'.format(args.topic), '').split('/')
-    arg = None
-    cmd = suffix[0]
-    if len(suffix) > 1:
-        arg = suffix[1]
-    logger.info("callback {} {}".format(cmd, arg))
-    if cmd in TOPIC_STATUS_PULSE:
-        device(int(arg))
-    elif cmd in TOPIC_STATUS_ON:
-        device(-1)
-    elif cmd in TOPIC_STATUS_OFF:
-        device(0)
+def my_callback(client, userdata, message):
+    print("sub {} {}".format(message.topic, message.payload))
+    if message.topic == args.topic:
+        # base topic
+        print("base topic {}".format(message.topic))
     else:
-        logging.warning('callback unrecognized command: {}'.format(cmd))
+        # sub topic
+        suffix = message.topic.replace('{}/'.format(args.topic), '').split('/')
+        arg = None
+        cmd = suffix[0]
+        if len(suffix) > 1:
+            arg = suffix[1]
+        print("sub topic {} {}".format(cmd, arg))
 
 
 if __name__ == "__main__":
+    # Read in command-line parameters
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host",
                         help="Your AWS IoT custom endpoint")
     parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
     parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
     parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
-    parser.add_argument("--port", action="store", dest="port", type=int, help="Port number override")
+    parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
     parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
                         help="Use MQTT over WebSocket")
     parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
                         help="Targeted client id")
     parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
-    parser.add_argument("--thing", help="thing name", default=platform.node().split('.')[0])
-    parser.add_argument("-p", "--pin", help="gpio pin (using BCM numbering)", type=int)
-    parser.add_argument("-x", "--on_time", help="Number of seconds on", type=float, default=1)
-    parser.add_argument("-y", "--off_time", help="Number of seconds off", type=float, default=1)
-    parser.add_argument("-z", "--default", help="Pattern 0=off, -1=on, 1..n=number of blinks", type=int, default=1)
     args = parser.parse_args()
 
     port = args.port
@@ -78,13 +51,6 @@ if __name__ == "__main__":
         port = 443
     if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
         port = 8883
-
-    # Configure logging
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger('output_sub')
-    logger.addHandler(watchtower.CloudWatchLogHandler(args.thing))
-
-    output = DigitalOutputDevice(args.pin)
 
     # Init AWSIoTMQTTClient
     myAWSIoTMQTTClient = None
@@ -105,7 +71,10 @@ if __name__ == "__main__":
     myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
     myAWSIoTMQTTClient.connect()
-    logger.info('subscribing to {}/#'.format(args.topic))
+    # subscribe to base topic
+    myAWSIoTMQTTClient.subscribe('{}', 1, my_callback)
+    time.sleep(2)
+    # subscribe to subtopics
     myAWSIoTMQTTClient.subscribe('{}/#'.format(args.topic), 1, my_callback)
     time.sleep(2)
 
