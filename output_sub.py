cecea176f482d16628904aed358b8b0f4cdf61e2
Steve Woolley <steve.woolley@me.com>
2018-12-30 22:54:02 -0500

    add output_sub

---
+++ output_sub.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import logging
+import watchtower
+import time
+import platform
+import argparse
+from gpiozero import DigitalOutputDevice
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+TOPIC_STATUS_ON = ['1', 'on']
+TOPIC_STATUS_OFF = ['0', 'off']
+TOPIC_STATUS_TOGGLE = ['toggle']
+TOPIC_STATUS_PULSE = ['blink', 'pulse']
+
+
+def device(cmd):
+    logging.debug("device {}".format(cmd))
+    if args.pin is not None:
+        if cmd < 0:
+            output.on()
+        elif cmd == 0:
+            output.off()
+        elif cmd > 0:
+            output.blink(args.on_time, args.off_time, cmd)
+
+
+def my_callback(client, user_data, message):
+    logger.debug("callback message {} {}".format(message.topic, message.payload))
+    suffix = message.topic.replace('{}/'.format(args.topic), '').split('/')
+    arg = None
+    cmd = suffix[0]
+    if len(suffix) > 1:
+        arg = suffix[1]
+    logger.info("callback {} {}".format(cmd, arg))
+    if cmd in TOPIC_STATUS_PULSE:
+        device(int(arg))
+    elif cmd in TOPIC_STATUS_ON:
+        device(-1)
+    elif cmd in TOPIC_STATUS_OFF:
+        device(0)
+    else:
+        logging.warning('callback unrecognized command: {}'.format(cmd))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--endpoint", required=True, help="Your AWS IoT custom endpoint")
+    parser.add_argument("-r", "--rootCA", required=True, help="Root CA file path")
+    parser.add_argument("-c", "--cert", required=True, help="Certificate file path")
+    parser.add_argument("-k", "--key", required=True, help="Private key file path")
+    parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
+    parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                        help="Use MQTT over WebSocket")
+    parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
+                        help="Targeted client id")
+    parser.add_argument("-t", "--topic", required=True, help="MQTT topic(s)")
+    parser.add_argument("--thing", help="thing name", default=platform.node().split('.')[0])
+    parser.add_argument("-p", "--pin", help="gpio pin (using BCM numbering)", type=int)
+    parser.add_argument("-x", "--on_time", help="Number of seconds on", type=float, default=1)
+    parser.add_argument("-y", "--off_time", help="Number of seconds off", type=float, default=1)
+    parser.add_argument("-z", "--default", help="Pattern 0=off, -1=on, 1..n=number of blinks", type=int, default=1)
+    args = parser.parse_args()
+
+    port = args.port
+
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('output_sub')
+    logger.addHandler(watchtower.CloudWatchLogHandler(args.thing))
+
+    output = DigitalOutputDevice(args.pin)
+
+    # Init AWSIoTMQTTClient
+    myAWSIoTMQTTClient = None
+    if args.useWebsocket:
+        myAWSIoTMQTTClient = AWSIoTMQTTClient(args.clientId, useWebsocket=True)
+        myAWSIoTMQTTClient.configureEndpoint(args.host, port)
+        myAWSIoTMQTTClient.configureCredentials(args.rootCAPath)
+    else:
+        myAWSIoTMQTTClient = AWSIoTMQTTClient(args.clientId)
+        myAWSIoTMQTTClient.configureEndpoint(args.host, port)
+        myAWSIoTMQTTClient.configureCredentials(args.rootCAPath, args.privateKeyPath, args.certificatePath)
+
+    # AWSIoTMQTTClient connection configuration
+    myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+    myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+    myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+    myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+    myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+    myAWSIoTMQTTClient.connect()
+    myAWSIoTMQTTClient.subscribe('{}/#'.format(args.topic), 1, my_callback)
+    time.sleep(2)
+
+    while True:
+        time.sleep(1)
