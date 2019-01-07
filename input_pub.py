ff9c1ed3824d028c1548b8490b80a8763d3d5193
Steve Woolley <steve.woolley@me.com>
2019-01-07 14:22:49 -0500

    include timestamp in log files

--- input_pub.py
+++ input_pub.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
         exit(2)
 
     # Configure logging
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format=piot.LOG_FORMAT)
     logger = logging.getLogger('input_pub')
     logger.addHandler(watchtower.CloudWatchLogHandler(args.thing))
 
--- piot.py
+++ piot.py
@@ -5,6 +5,9 @@ TOPIC_STATUS_ON = ['1', 'on']
 TOPIC_STATUS_OFF = ['0', 'off']
 TOPIC_STATUS_TOGGLE = ['toggle']
 TOPIC_STATUS_PULSE = ['blink', 'pulse']
+LOG_FORMAT = '%(asctime)s %(filename)-15s %(funcName)-15s %(levelname)-8s %(message)s'
+DATE_FORMAT = '%Y/%m/%d %-I:%M %p %Z'
+FILE_DATE_FORMAT = '%Y-%m-%d-%H-%M-%S'
 
 
 def iot_thing_topic(thing):
