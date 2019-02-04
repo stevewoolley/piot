3db61cdfe7ba615a3ac407000f96a409e7af9a6d
Steve Woolley <steve.woolley@me.com>
2019-02-04 16:38:37 -0500

    add time window

--- snapshot.py
+++ snapshot.py
@@ -5,6 +5,9 @@ import datetime
 from picamera import PiCamera
 import argparse
 import platform
+import logging
+import piot
+import watchtower
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,13 +15,28 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--prefix", action="store", default='snapshot', dest="prefix", help="Output prefix")
     parser.add_argument("--width", action="store", default=3280, type=int, dest="width", help="Resolution width")
     parser.add_argument("--height", action="store", default=2464, type=int, dest="height", help="Resolution height")
+    parser.add_argument("--nix_before", action="store", type=int, dest="nix_before", help="Don't snap before hour")
+    parser.add_argument("--nix_after", action="store", type=int, dest="nix_after", help="Don't snap after hour")
     parser.add_argument("--thing", help="thing name", default=platform.node().split('.')[0])
     args = parser.parse_args()
 
-    camera = PiCamera()
-    camera.resolution = (args.width, args.height)
-    sleep(2)  # Camera warm-up time
-    output = "{}-{}-{}.{}".format(args.prefix, args.thing, datetime.datetime.now().strftime('%Y%m%d%H%M%S'),
-                                  args.format)
-    camera.capture(output, format=args.format)
-    camera.close()
+    # Configure logging
+    logging.basicConfig(level=logging.INFO, format=piot.LOG_FORMAT)
+    logger = logging.getLogger('snapshot')
+    logger.addHandler(watchtower.CloudWatchLogHandler(args.thing))
+
+    if args.nix_before and datetime.datetime.now().hour < args.nix_before:
+        logger.warn(
+            "snapshot cancelled before {} {}".format(args.nix_before, datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
+    elif args.nix_after and datetime.datetime.now().hour >= args.nix_after:
+        logger.warn(
+            "snapshot cancelled after {} {}".format(args.nix_after, datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
+    else:
+        camera = PiCamera()
+        camera.resolution = (args.width, args.height)
+        sleep(2)  # Camera warm-up time
+        output = "{}-{}-{}.{}".format(args.prefix, args.thing, datetime.datetime.now().strftime('%Y%m%d%H%M%S'),
+                                      args.format)
+        logger.info("snapshot {}".format(output))
+        camera.capture(output, format=args.format)
+        camera.close()
