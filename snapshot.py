e0fcc52ba905e566c075f8f1624d6b6a8f68aebb
Steve Woolley <steve.woolley@me.com>
2019-02-04 16:24:25 -0500

    detail output filename

--- snapshot.py
+++ snapshot.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 
 from time import sleep
+import datetime
 from picamera import PiCamera
 import argparse
-
+import platform
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--output", action="store", required=True, dest="output", help="Output filename")
     parser.add_argument("-f", "--format", action="store", default='jpeg', dest="format", help="Output format")
+    parser.add_argument("-p", "--prefix", action="store", default='snapshot', dest="prefix", help="Output prefix")
     parser.add_argument("--width", action="store", default=3280, type=int, dest="width", help="Resolution width")
     parser.add_argument("--height", action="store", default=2464, type=int, dest="height", help="Resolution height")
+    parser.add_argument("--thing", help="thing name", default=platform.node().split('.')[0])
     args = parser.parse_args()
 
     camera = PiCamera()
     camera.resolution = (args.width, args.height)
-    # Camera warm-up time
-    sleep(2)
+    sleep(2)  # Camera warm-up time
+    output = "{}-{}-{}.{}".format(args.prefix, args.thing, datetime.datetime.now().strftime('%Y%m%d%H%M%S'),
+                                  args.format)
     camera.capture(args.output, format=args.format)
     camera.close()
