6aa1330dc6792265192a668e5dc9920f55304b14
Steve Woolley <steve.woolley@me.com>
2019-02-04 16:50:27 -0500

    put snapshots in a directory

--- snapshot.py
+++ snapshot.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--format", action="store", default='jpeg', dest="format", help="Output format")
     parser.add_argument("-p", "--prefix", action="store", default='snapshot', dest="prefix", help="Output prefix")
+    parser.add_argument("-d", "--directory", action="store", default='/tmp', dest="directory", help="Output directory")
     parser.add_argument("--width", action="store", default=3280, type=int, dest="width", help="Resolution width")
     parser.add_argument("--height", action="store", default=2464, type=int, dest="height", help="Resolution height")
     parser.add_argument("--nix_before", action="store", type=int, dest="nix_before", help="Don't snap before hour")
@@ -35,7 +36,7 @@ if __name__ == "__main__":
         camera = PiCamera()
         camera.resolution = (args.width, args.height)
         sleep(2)  # Camera warm-up time
-        output = "{}-{}-{}.{}".format(args.prefix, args.thing, datetime.datetime.now().strftime('%Y%m%d%H%M%S'),
+        output = "{}/{}-{}-{}.{}".format(args.directory, args.prefix, args.thing, datetime.datetime.now().strftime('%Y%m%d%H%M%S'),
                                       args.format)
         logger.info("snapshot {}".format(output))
         camera.capture(output, format=args.format)
